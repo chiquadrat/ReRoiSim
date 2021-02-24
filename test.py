6c6e1e8d9dc7a1060cdd55b7110756d2c05c4fdd
Paul Walter <chiquadrat@users.noreply.github.com>
2021-02-24 18:46:03 +0100

    Added three unit tests

--- test.py
+++ test.py
@@ -3,14 +3,30 @@ from formeln import renditerechner
 
 class TestSum(unittest.TestCase):
 
-    def test_steuerberechnung_szenario_1(self):
+    def test_steuerberechnung_default(self):
         vk, ekren, objren = renditerechner()
         self.assertEqual(vk, 209177, "Should be 209177")
         self.assertEqual(ekren, 0.0943, "Should be 9.43%")
         self.assertEqual(objren, 0.0487, "Should be 4.87%")
 
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 5, "Should be 6")
+    def test_steuerberechnung_szenario_1(self):
+        vk, ekren, objren = renditerechner(
+            alleinstehend=True,  
+        )
+        self.assertEqual(vk, 209177, "Should be 209177")
+        self.assertEqual(ekren, 0.091, "Should be 9.14%")
+        self.assertEqual(objren, 0.0467, "Should be 4.67%")
+        
+    def test_steuerberechnung_szenario_2(self):
+        vk, ekren, objren = renditerechner(
+            baujahr=1918, 
+            verkaufsfaktor=20,
+        )
+        self.assertEqual(vk, 167341, "Should be 167342")
+        self.assertEqual(ekren, 0.088, "Should be 8.85%")
+        self.assertEqual(objren, 0.0444, "Should be 4.44%")
+
+
 
 if __name__ == '__main__':
     unittest.main()
