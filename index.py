b28780028fc9106e030fac71453649e8e4cba890
Paul Walter <chiquadrat@users.noreply.github.com>
2022-01-01 12:38:42 +0100

    changed folder structure

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-from apps import app_mieten_vs_kaufen, app_immo_kapitalanlage
+import app_mieten_vs_kaufen, app_immo_kapitalanlage
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
@@ -13,18 +13,18 @@ app.layout = html.Div([
 
 index_page = html.Div([
     html.H1('Simuliers!'),
-    dcc.Link('Immobilienrendite', href='/apps/app_immo_kapitalanlage'),
+    dcc.Link('Immobilienrendite', href='/app_immo_kapitalanlage'),
     html.Br(),
-    dcc.Link('Mieten vs. Kaufen', href='/apps/app_mieten_vs_kaufen'),
+    dcc.Link('Mieten vs. Kaufen', href='/app_mieten_vs_kaufen'),
 ])
 
 
 @app.callback(Output('page-content', 'children'),
               Input('url', 'pathname'))
 def display_page(pathname):
-    if pathname == '/apps/app_immo_kapitalanlage':
+    if pathname == '/app_immo_kapitalanlage':
         return app_immo_kapitalanlage.layout
-    if pathname == '/apps/app_mieten_vs_kaufen':
+    if pathname == '/app_mieten_vs_kaufen':
         return app_mieten_vs_kaufen.layout
     else:
         return index_page
