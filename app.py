73b0f1c7fa357cf8763b027cfa5a6739d4fd1976
Paul Walter <chiquadrat@users.noreply.github.com>
2022-01-01 12:52:47 +0100

    Secret key

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+import os
 
 app = dash.Dash(__name__)
 server = app.server
-#app.config.suppress_callback_exceptions = True
+app.config.suppress_callback_exceptions = True
+server.secret_key = os.environ.get('secret_key', 'secret')
